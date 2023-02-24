8553f93de102a8ae536e5b4b96922dfcd3ae8c70
Lucas <lucasviniciuslimadepaula@gmail.com>
2023-02-24 12:19:32 -0300

    Correção de bugs, alteraçõões no estilo, implementação da biblioteca AXIOS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,50 @@
-const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const BASE_URL = "https://api.thecatapi.com/v1/images/search";
 
-const getCats = async () => {
-    try {
-        const data = await fetch(BASE_URL); 
-        const json = await data.json();
-        return json.webpurl;
-    } catch (e) {
-        console.log(e.message);        
-    }
-};
+// Alternativa Usando AXIOS 
 
-const loadImg = async () => {
-    const img = document.getElementsByTagName('img')[0];
-	img.src = await getCats();
+const promessa = axios.get(BASE_URL)
+promessa.then(exibeImagem)
+const elemento = document.querySelector("#cat")
+
+function exibeImagem(resposta){
+    elemento.src = resposta.data[0].url
 }
-
-loadImg();
-
+   
 const btn = document.getElementById('change-cat');
-btn.addEventListener('click', loadImg);
+btn.addEventListener('click', trocarImg);
  
+function trocarImg() {
+    const promessa = axios.get(BASE_URL);
+    promessa.then(exibeImagem);
+  }
+
+
+
+//  Alternativa Sem Usar AXIOS 
+
+
+
+// const getCats = async () => {
+//     try {
+//         const data = await fetch(BASE_URL); 
+//         const json = await data.json();
+//         return json[0].url
+//     } catch (e) {
+//         console.log(e.message);        
+//     }
+// };
+
+// const loadImg = async () => {
+//     const img = document.getElementsByTagName('img')[0];
+// 	img.src = await getCats();
+    
+// }
+
+// loadImg();
+
+// const btn = document.getElementById('change-cat');
+// btn.addEventListener('click', loadImg);
+
+
+
+
